03c79d1e8e114ddb65d7eb52be26700dd206cda2
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2022-03-29 02:51:03 +0530

    New code from Reference: https://medium.com/swlh/build-your-first-rest-api-with-django-rest-framework-e394e39a482c

--- mysite/myapi/urls.py
+++ mysite/myapi/urls.py
@@ -1,10 +1,11 @@
+# myapi/urls.py
+
 from django.urls import include, path
 from rest_framework import routers
-from backendcode.quickstart import views
+from . import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'heroes', views.HeroViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
