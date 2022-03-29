d18ca24fb4fcb5a236109ccf2e6b379e04fd4f22
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2022-03-29 23:01:26 +0530

    Update admin.py

--- mysite/myapi/admin.py
+++ mysite/myapi/admin.py
@@ -1,5 +1,23 @@
-from django.contrib import admin
-from .models import Hero, StudentDetail
+# models.py
+from django.db import models
 
-admin.site.register(Hero)
-admin.site.register(StudentDetail)
+class Hero(models.Model):
+    name = models.CharField(max_length=60)
+    alias = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.name
+
+
+class StudentDetail(models.Model):
+    firstname = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+    grade = models.PositiveIntegerField()
+    mailid = models.CharField(max_length=200)
+    contactnumber = models.CharField(max_length=60)
+    adharnumber = models.CharField(max_length=60)
+    address = models.CharField(max_length=200)
+    vaccinationstatus = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return "%s, %s" % (self.lastname, self.firstname)
